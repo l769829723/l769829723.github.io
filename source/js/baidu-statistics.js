d1c7bdca8834ceb3074fcddf8c19b17a5501980d
opsland <1054473703@qq.com>
2020-03-08 17:07:02 +0800

    add statistics script

---
+++ source/js/baidu-statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6e9868ff065be484390979c7a76b6b34";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
